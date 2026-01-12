d8a1e85cd472d81245d7c489c0140b6b33353a08
Patseroni <patrik.bustad@lerniastudent.se>
2026-01-12 22:48:12 +0100

    add basic header

--- src/client/components/Menu.tsx
+++ src/client/components/Menu.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import styles from '../styles/Menu.module.css'
 
 function Menu() {
     return (
         <>
-            <nav>
+            <nav className={styles.menu}>
                 <Link to='/profile'>Profile</Link>
                 <Link to='/register'>Register</Link>
                 <Link to='/login'>Log in</Link>
